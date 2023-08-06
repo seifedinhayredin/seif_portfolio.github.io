b5da95c8e142453d109e3dde45d226ec73f9b3db
seifedinhayredin <seifedinhayredin@gmail.com>
2023-08-06 19:39:55 +0300

    update

---
+++ src/Car.jsx
@@ -0,0 +1,8 @@
+function Car(props) {
+  return (
+    <li>
+      My car is <i>{props.brand}</i>
+    </li>
+  );
+}
+export default Car;
--- src/Garage.jsx
+++ src/Garage.jsx
@@ -1,24 +1,7 @@
-import Car from "./Car";
-import Football from "./Football";
-import GroceryLists from "./GroceryLists";
-import DeptList from "./DeptList";
 function Garage() {
-  const cars = [
-    { id: 1, brand: "Toyota" },
-    { id: 2, brand: "BMW" },
-    { id: 3, brand: "Volsewagen" },
-  ];
-
   return (
     <>
       <h1>These are types of cars</h1>
-      <ul>
-        {cars.map((car) => (
-          <Car key={car.id} brand={car.brand} />
-        ))}
-      </ul>
-      <GroceryLists />
-      <DeptList />
     </>
   );
   /* const myCar = { name: "Toyota", color: "red" };
