d41ee6cd165ea18a0b5a8f89aec29ba0567e475c
seifedinhayredin <seifedinhayredin@gmail.com>
2023-08-06 16:48:03 +0300

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Blog from "./pages/Blog";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Layout from "./pages/Layout";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
@@ -11,22 +11,20 @@ import Comp1 from "./pages/Comp1";
 
 function Rou() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route exact path="blog" element={<Blog />} />
-            <Route exact path="contact" element={<Contact />} />
-            <Route exact path="about" element={<About />} />
-            <Route exact path="comp1" element={<Comp1 />} />
-            <Route path="*" element={<NoPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="about" element={<About />} />
+          <Route path="comp1" element={<Comp1 />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   );
 }
-export default Rou;
+
 const root = ReactDOM.createRoot(document.getElementById("seif"));
 root.render(<Rou />);
